eb92c1642ec43175175ad3f4ca1b1fa756ce0c87
Michał Kowol <michal.kowol@gmail.com>
2026-05-16 10:11:33 +0200

    Adds logging to OrderFacade and PizzaFacade

--- src/main/kotlin/eu/codeloop/ai/wjug/application/order/OrderFacade.kt
+++ src/main/kotlin/eu/codeloop/ai/wjug/application/order/OrderFacade.kt
@@ -7,6 +7,7 @@ import eu.codeloop.ai.wjug.domain.order.OrderNotFoundException
 import eu.codeloop.ai.wjug.domain.order.OrderRepository
 import eu.codeloop.ai.wjug.domain.pizza.PizzaNotFoundException
 import eu.codeloop.ai.wjug.domain.pizza.PizzaRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,20 +16,27 @@ class OrderFacade(
     private val pizzaRepository: PizzaRepository,
     private val idGenerator: IdGenerator
 ) {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
     fun findAll(): List<Order> {
+        log.info("Finding all orders")
         return orderRepository.findAll()
     }
 
     fun getById(id: OrderId): Order {
+        log.info("Getting order by id [id={}]", id)
         return orderRepository.findById(id) ?: throw OrderNotFoundException(id)
     }
 
     fun create(command: CreateOrderCommand): Order {
+        log.info("Creating order [pizzaIds={}]", command.pizzaIds)
         command.pizzaIds.forEach { pizzaId ->
             pizzaRepository.findById(pizzaId) ?: throw PizzaNotFoundException(pizzaId)
         }
         val order = Order(id = OrderId(idGenerator.generate()), pizzaIds = command.pizzaIds)
         orderRepository.save(order)
+        log.info("Order created [id={}]", order.id)
         return order
     }
 }
--- src/main/kotlin/eu/codeloop/ai/wjug/application/pizza/PizzaFacade.kt
+++ src/main/kotlin/eu/codeloop/ai/wjug/application/pizza/PizzaFacade.kt
@@ -4,17 +4,23 @@ import eu.codeloop.ai.wjug.domain.pizza.Pizza
 import eu.codeloop.ai.wjug.domain.pizza.PizzaId
 import eu.codeloop.ai.wjug.domain.pizza.PizzaNotFoundException
 import eu.codeloop.ai.wjug.domain.pizza.PizzaRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class PizzaFacade(
     private val pizzaRepository: PizzaRepository
 ) {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
     fun findAll(): List<Pizza> {
+        log.info("Finding all pizzas")
         return pizzaRepository.findAll()
     }
 
     fun getById(id: PizzaId): Pizza {
+        log.info("Getting pizza by id [id={}]", id)
         return pizzaRepository.findById(id) ?: throw PizzaNotFoundException(id)
     }
 }
