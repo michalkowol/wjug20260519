a43e9e8625cb9dc5e5f13c31ab3c863d4ac9a3cf
Michał Kowol <michal.kowol@gmail.com>
2026-05-16 07:16:37 +0200

    Spring Initializr

---
+++ src/main/kotlin/eu/codeloop/ai/wjug/WjugApplication.kt
@@ -0,0 +1,11 @@
+package eu.codeloop.ai.wjug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WjugApplication
+
+fun main(args: Array<String>) {
+	runApplication<WjugApplication>(*args)
+}
---
+++ src/test/kotlin/eu/codeloop/ai/wjug/WjugApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.codeloop.ai.wjug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WjugApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
